80aab49093204d5c09de59dd0f4ec564f23f9463
enzinbala <167486665+enzinbala@users.noreply.github.com>
2024-06-07 10:00:33 -0300

    Subindo arquivos!

---
+++ stnovo.js
@@ -0,0 +1,23 @@
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('#header')
+    header.classList.toggle('rolagem', window.scrollY > 600)
+})
+
+/*CONTADOR*/
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
+AOS.init();
